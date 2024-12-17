d7e60894769ca64bb48e6f90e435e08c870f28f7
unknown <you@example.com>
2024-12-18 01:33:27 +0500

    first commit

---
+++ app/routers/__init__.py
@@ -0,0 +1,4 @@
+from .task import router as task_router
+from .user import router as user_router
+
+__all__ = ["task_router", "user_router"]
---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.routers import task, user
+
+
+app = FastAPI()
+
+
+app.include_router(task.router)
+app.include_router(user.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Taskmanager"}
